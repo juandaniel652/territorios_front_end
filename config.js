e9c17d922b367356efe8b365c9d4594de1ca54fa
Juan Daniel <juankycabj05@gmail.com>
2025-12-20 17:35:49 -0300

    Configuracion adaptable a local y para produccion inclusive

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 // ===============================
 // config.js: Responsabilidad: configuración global (URLs, flags, etc.)
 // ===============================
+const isLocal = window.location.hostname === "localhost" ||
+                window.location.hostname === "127.0.0.1";
+
 export const CONFIG = {
-    BASE_URL: "http://127.0.0.1:8000" //para la produccion BASE_URL: "https://backend-territorios.onrender.com"
+  BASE_URL: isLocal
+    ? "http://127.0.0.1:8000"
+    : "https://backend-territorios.onrender.com"
 };
