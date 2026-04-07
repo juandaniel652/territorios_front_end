9f4586031530cf5cea126ea0b563dba493980282
Juan Daniel <juankycabj05@gmail.com>
2026-04-06 21:25:36 -0300

    fix: agregar id en constructor de Asignacion

--- dashboard/domain/entities/Asignacion.js
+++ dashboard/domain/entities/Asignacion.js
@@ -1,11 +1,11 @@
 // domain/entities/Asignacion.js
 export class Asignacion {
     constructor({ id, numero_territorio, conductor, fecha_asignado, fecha_completado, cantidad_abarcado }) {
-        this.id               = id ?? null;  // ← agregar
+        this.id               = id ?? null;
         this.numero_territorio = numero_territorio;
         this.conductor         = conductor;
         this.fecha_asignado    = fecha_asignado;
         this.fecha_completado  = fecha_completado;
-        this.cantidad_abarcado    = cantidad_abarcado ?? null;
+        this.cantidad_abarcado = cantidad_abarcado ?? null;
     }
 }
