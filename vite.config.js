a8be97d95fe3e4a5d09a099b8d9da4f8b4c6dea8
Juan Daniel <juankycabj05@gmail.com>
2026-04-11 14:55:23 -0300

    vite con aaptacion par index.html de gestor

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'login/index.html'),
+        main: resolve(__dirname, 'index.html'), // Tu nuevo index de la raíz
+        login: resolve(__dirname, 'login/index.html'),
         dashboard: resolve(__dirname, 'dashboard/index.html')
       }
     }
