8b185638f6e18310d629390697e6d92d12bfae26
Juan Daniel <juankycabj05@gmail.com>
2026-04-11 23:43:35 -0300

    vercel.jso y vite.config.js adaptados a nuevo moudlo register.html

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  root: './', // Asegúrate de que apunte a la raíz de tu frontend
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'), // Tu nuevo index de la raíz
+        main: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'login/index.html'),
         register: resolve(__dirname, 'login/register.html'),
         dashboard: resolve(__dirname, 'dashboard/index.html')
